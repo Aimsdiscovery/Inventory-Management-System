0bb491b292b1b9bd7e65d086974d0cec0c2b7527
Aimsdiscovery <maimanhasrin@gmail.com>
2025-10-22 09:45:31 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const http = require("http");
+
+// Create a server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, world! Welcome to my Node app.");
+});
+
+// Run the server on port 3000
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
