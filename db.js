f5ea8a71a4a52822aa974739cffbc150c83485b7
Aimsdiscovery <maimanhasrin@gmail.com>
2025-11-15 03:27:19 +0800

    patch 2.1

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'aimanezz170202A@', // change if needed
-  database: 'inventory_db', // change this
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
